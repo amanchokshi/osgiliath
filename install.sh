4c2f50b00a8dfae6db34f68ca43342e658e56902
Aman Chokshi <amanchokshi.iisc@gmail.com>
2020-07-20 13:25:36 +1000

    Added flake8 config

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 # Stow dotfiles to appropriate locations
 
-echo 'Stowing .zshrc to "$HOME"'
+echo "Stowing .zshrc to '$HOME'"
 stow -v -R -t ~ zsh
 
-echo 'Stowing neovim config to "$HOME"'
+echo "Stowing neovim config to '$HOME'"
 stow -v -R -t ~ nvim
 
-echo 'Stowing alacritty config to "$HOME"'
+echo "Stowing alacritty config to '$HOME'"
 stow -v -R -t ~ alacritty
 
-echo 'Stowing tmux config to "$HOME"'
+echo "Stowing tmux config to '$HOME'"
 stow -v -R -t ~ tmux
+
+echo "Stowing flake8 config to '$HOME'"
+stow -v -R -t ~ flake8
