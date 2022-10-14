b327c13936a5d933ddd94094a45b08aaea14b482
Aman Chokshi <amanchokshi.iisc@gmail.com>
2022-10-14 16:08:20 +1300

    Commit dump of current nvim + alacritty state

--- nvim/.config/nvim/lua/dashboard-config/init.lua
+++ nvim/.config/nvim/lua/dashboard-config/init.lua
@@ -8,7 +8,7 @@ vim.g.dashboard_custom_section = {
   a = {description = {'  Find File          '}, command = 'Telescope find_files'},
   d = {description = {'  Search Text        '}, command = 'Telescope live_grep'},
   b = {description = {'  Recent Files       '}, command = 'Telescope oldfiles'},
-  e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'}
+  -- e = {description = {'  Config             '}, command = 'edit ~/.config/nvim/init.lua'}
 }
 
 -- Programmer Quote `https://github.com/Th3Whit3Wolf/pquote`
