e64b0396bf10f08b11dd3754ee927b96adc94b10
Aman Chokshi <amanchokshi.iisc@gmail.com>
2022-10-14 16:43:16 +1300

    Fixed for changed in new nvim-tree

--- nvim/.config/nvim/lua/nvim-tree-config/init.lua
+++ nvim/.config/nvim/lua/nvim-tree-config/init.lua
@@ -1,29 +1,30 @@
-vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '➜',
-    git = {
-      	unstaged = "!",
-      	staged = "⁺",
-      	unmerged = "",
-      	renamed = "»",
-      	untracked = "*",
-      	deleted = "ˣ",
-      	ignored = "ᴼ"
-      },
-    folder = {
-      arrow_open = "⬧",
-      arrow_closed = "❯",
-      default = "",
-      open = "",
-      empty = "",
-      empty_open = "",
-      symlink = "",
-      symlink_open = ""
-      }
-}
-
-vim.g.nvim_tree_root_folder_modifier = ":t:r"
-
 require'nvim-tree'.setup {
-  auto_close = true
+  renderer = {
+      root_folder_modifier = ":t:r",
+      icons = {
+          glyphs = {
+            default = '',
+            symlink = '➜',
+            git = {
+                unstaged = "!",
+                staged = "⁺",
+                unmerged = "",
+                renamed = "»",
+                untracked = "*",
+                deleted = "ˣ",
+                ignored = "ᴼ"
+              },
+            folder = {
+              arrow_open = "⬧",
+              arrow_closed = "❯",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = ""
+              }
+          }
+      }
+  }
 }
